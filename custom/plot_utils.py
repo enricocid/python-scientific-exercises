f0943f7734eb2ab14bb665950fc11df505e9dc50
enricocid <enrico2588@gmail.com>
2022-01-01 21:35:47 +0100

    Add particle in a 1D box example
    Signed-off-by: enricocid <enrico2588@gmail.com>

--- custom/plot_utils.py
+++ custom/plot_utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 from pandas import to_datetime
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import AnchoredText
 
 
 def get_xticks_labels(reports_dates, full=False):
@@ -18,3 +20,67 @@ def get_xticks_labels(reports_dates, full=False):
         labels[0] = ""
     labels = list(map(str.title, labels))
     return ticks, labels
+
+
+def plot_1dbox_infinite_walls(b_color="tab:green", w_color="tab:blue"):
+
+    L = 10
+    step = L/2
+
+    fig, ax = plt.subplots()
+
+    # remove spines
+    for _, spine in ax.spines.items():
+        spine.set_visible(False)
+    ax.set_yticks([])
+
+    # set plot limits
+    ax.set_ylim(0,)
+    ax.set_xlim(-L, L*2)
+    _, ymax = ax.get_ylim()
+
+    # set ticks labels
+    zeros = [0, L]
+    zero_labels = dict(zip(zeros, ["0", "L"]))
+    xticks = np.arange(-10, L*2+step, step)
+    xlabels = ["" if x not in zeros else zero_labels[x] for x in xticks]
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(xlabels)
+
+    # draw axis with arrow
+
+    # vertical axis
+    arrowprops = dict(arrowstyle="->",
+                      linewidth=1.5,
+                      color=w_color)
+    for zero in zeros:
+        ax.annotate("", xy=(zero, ymax), xytext=(zero, 0), arrowprops=arrowprops)
+
+    # abscissa
+    arrowprops_ = dict(arrowstyle="<->",
+                       linewidth=1,
+                       color="k")
+    ax.annotate("", xy=(-L, 0), xytext=(L*2, 0), arrowprops=arrowprops_)
+
+    # highlight the box
+    ax.axvspan(xmin=0, xmax=L, color=b_color, alpha=0.01)
+
+    # highlight the walls
+    ax.axvspan(xmin=L, xmax=L*2, color=w_color, alpha=0.10)
+    ax.axvspan(xmin=0, xmax=-L, color=w_color, alpha=0.10)
+
+    # add potential values
+    props = dict(fontweight="bold")
+    # inside the box
+    at = AnchoredText("V=0", frameon=False, loc="lower center", prop=props)
+    ax.add_artist(at)
+
+    # walls
+    at1 = AnchoredText("V=\u221e", frameon=False, loc="center left", pad=step, prop=props)
+    ax.add_artist(at1)
+
+    at2 = AnchoredText("V=\u221e", frameon=False, loc="center right", pad=step, prop=props)
+    ax.add_artist(at2)
+
+    fig.tight_layout()
+    plt.show()
